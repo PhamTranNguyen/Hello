283613699984acfa723f2b1c9edaac5de9ebb9b1
PhamTranNguyen <133732480+PhamTranNguyen@users.noreply.github.com>
2023-05-16 11:29:07 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-def video():
-	st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+def data():
+	st.text('thank')
 
 if st.button("pls click"):
 	st.session_state.count += 1
-	video()
+	data()
   
