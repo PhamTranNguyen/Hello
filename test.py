7cb87728ae38fce94fb01ac5117a05f8244240bd
PhamTranNguyen <133732480+PhamTranNguyen@users.noreply.github.com>
2023-05-16 11:26:52 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-st.button('pls click')
-st.text('nice')
+def video():
+	st.video(https://www.youtube.com/watch?v=dQw4w9WgXcQ)
+
+if st.button("Add new row"):
+	st.session_state.count += 1
+	video()
+  
