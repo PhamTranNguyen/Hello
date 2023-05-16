e5a94a2789073acf5b41b32ccb3e3ac4810abbd2
PhamTranNguyen <133732480+PhamTranNguyen@users.noreply.github.com>
2023-05-16 11:27:22 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def video():
-	st.video(https://www.youtube.com/watch?v=dQw4w9WgXcQ)
+	st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
 if st.button("Add new row"):
 	st.session_state.count += 1
